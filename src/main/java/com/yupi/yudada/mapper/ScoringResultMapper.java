ec6ac90bbd4f196b57f8828352e235a094dda912
“lizefeng” <280796942@qq.com>
2024-06-18 23:00:32 +0800

    dto

---
+++ src/main/java/com/yupi/yudada/mapper/ScoringResultMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yudada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.yudada.model.entity.ScoringResult;
+
+/**
+* @author Administrator
+* @description 针对表【scoring_result(评分结果)】的数据库操作Mapper
+* @createDate 2024-06-16 17:24:18
+* @Entity generator.domain.ScoringResult
+*/
+public interface ScoringResultMapper extends BaseMapper<ScoringResult> {
+
+}
+
+
+
+
